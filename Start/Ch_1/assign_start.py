2849052d75afe4e99ac7090ff82c3a086ae329ed
Ariadna Blanca Romero <ariadna@starignus.com>
2026-01-29 17:30:15 +0000

    add code

--- Start/Ch_1/assign_start.py
+++ Start/Ch_1/assign_start.py
@@ -10,8 +10,18 @@ print(x)
 
 # TODO: the assignment operator is part of an expression
 
+(x := 10) # helps to the readability 
+print(x)
 
 # TODO: The assignment expression is useful for writing concise code
+# thestr = input("Value?:")
+# while thestr != "exit":
+#     print(f"You entered: {thestr}")
+#     thestr = input("Value?:")
+# can be rewritten using the walrus operator
+
+# while (thestr := input("Value?:")) != "exit":
+#     print(f"You entered: {thestr}")
 
 
 # TODO: The walrus operator can help reduce redundant function calls
@@ -21,3 +31,14 @@ val_data = {
     "total": sum(values),
     "average": sum(values) / len(values)
 }
+
+pprint.pp(val_data)
+
+
+# can be rewritten using the walrus operator
+val_data = {
+    "length": (n := len(values)),
+    "total": (t := sum(values)),
+    "average": t / n
+}
+pprint.pp(val_data)
--- Start/Ch_1/templstr_start.py
+++ Start/Ch_1/templstr_start.py
@@ -1,6 +1,7 @@
 # Example file for Advanced Python: Language Features by Joe Marini
 # demonstrate template string functions
 
+from string import Template
 
 # Usual string formatting with f-strings
 str1 = "Advanced Python: Language Features"
@@ -9,7 +10,13 @@ outputstr = f"You're watching {str1} by {str2}"
 print(outputstr)
 
 # TODO: create a template with placeholders
+templ = Template("You're watching ${title} by ${author}")
 
 # TODO: use the substitute method with keyword arguments
 
+str2 = templ.substitute(title="Advanced Python: Language Features", author="Joe Marini")
+print(str2)
 # TODO: use the substitute method with a dictionary
+data = {"title": "Advanced Python: Language Features", "author": "Joe Marini"}
+str3 = templ.substitute(data)
+print(str3)
